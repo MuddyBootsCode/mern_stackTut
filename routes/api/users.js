aab86ea31f88dfabdd5823271a11dbec49e706c9
MuddyBootsCode <firsttogo98@gmail.com>
2018-09-20 11:20:50 -0500

    user registartion route complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,35 +17,36 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @access  Public
 
 router.post('/register', (req, res) => {
-    const { email, name, avatar, password } = req.body
-    User.findOne({ email })
-        .then(user => {
-            if(user){
-                return res.status(400).json({email: 'Email Already Exists'})
-            } else {
 
-                const avatar = gravatar.url(email, {
-                    s: '200', //size
-                    r: 'pg', //rating
-                    d: 'mm' //default
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'mm' // Default
                 });
 
                 const newUser = new User({
-                    name: name,
-                    email: email,
+                    name: req.body.name,
+                    email: req.body.email,
                     avatar,
-                    password: password
+                    password: req.body.password
                 });
 
-                bcrypt.getSalt(10, (err, salt) => {
+                bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) throw err;
+                        if (err) throw err;
                         newUser.password = hash;
-                        newUser.save()
+                        newUser
+                            .save()
                             .then(user => res.json(user))
-                            .catch(err => console.log(err))
-                    })
-                })
+                            .catch(err => console.log(err));
+                    });
+                });
             }
         });
 });
