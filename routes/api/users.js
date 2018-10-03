47a21fb3f11b089fdb80db468ff06f7d5b5dfd48
MuddyBootsCode <firsttogo98@gmail.com>
2018-10-03 10:36:56 -0500

    Feature: Adding Validation to project

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load user model
 const User = require('../../models/User');
 
@@ -20,6 +24,13 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @access  Public
 
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
 
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -100,7 +111,7 @@ router.post('/login', (req, res) => {
 // @access  Private
 
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-    const { id, name, email, avatar} = req.user
+    const { id, name, email } = req.user
     res.json({
         id,
         name,
